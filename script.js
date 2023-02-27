80eac67c386afee966ecaa2731517af854520517
denosaur98 <109144817+denosaur98@users.noreply.github.com>
2023-02-28 01:43:10 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,80 @@
+startGame(16, 16, 40)
+
+function startGame(width, height, bombsCount) {
+  const field = document.querySelector('.field_btns')
+  const cellsCount = width * height
+  field.innerHTML = '<button class="btn"></button>'.repeat(cellsCount)
+  const cells = [...field.children]
+
+  let closedCount = cellsCount
+
+  const bombs = [...Array(cellsCount).keys()]
+  .sort(() => Math.random() - 0.5).slice(0, bombsCount)
+
+  field.addEventListener('click', (e) => {
+    if(e.target.tagName !== 'BUTTON') {
+      return
+    }
+    const index = cells.indexOf(e.target)
+    const column = index % width
+    const row = Math.floor(index / width)
+    open(row, column)
+  })
+
+  function isValid(row, column) {
+    return row >= 0 && row < height && column >= 0 && column < width
+  }
+
+  function getCount(row, column) {
+    let count = 0
+    for(let i = -1; i <= 1; i++) {
+      for(let j = -1; j <= 1; j++) {
+        if(isBomb(row + j, column + i)) {
+          count++
+        }
+      }
+    }
+    return count
+  }
+
+  function open(row, column) {
+    if(!isValid(row, column)) return
+    
+    const index = row * width + column
+    const cell = cells[index]
+
+    if(cell.disabled === true) return
+    cell.disabled = true
+
+    if(isBomb(row, column)) {
+      cell.innerHTML = '<div class="bomb"></div>'
+      alert('Проиграл')
+      return
+    }
+
+    closedCount--
+    if(closedCount <= bombsCount) {
+      alert('Выиграл!')
+      return
+    }
+
+    const count = getCount(row, column)
+    
+    if(count !== 0) {
+      cell.innerHTML = `<div class="notBomb">${count}</div>`
+      return
+    }
+    
+    for(let i = -1; i <= 1; i++) {
+      for(let j = -1; j <= 1; j++) {
+        open(row + j, column + i)
+      }
+    }
+  }
+
+  function isBomb(row, column) {
+    if(!isValid(row, column)) return false
+    const index = row * width + column
+    return bombs.includes(index)
+  } 
+}
